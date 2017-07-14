8997e8c208d7694a7868c133d25659045da258f8
Orieta Toro <orieta.toro@gmail.com>
2017-07-13 23:52:03 -0400

    seccion preguntas frecuentes funcionando-falta estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
 /* Holiii acá va tu código también */
 /*index2.html*/
 $(".button-collapse").sideNav();
+
+/*dropdown*/
+$('.dropdown_click').click(function() {
+  $('.dropdown').each(function() {
+    if ($(this).find('p').css('display') == 'block')
+      $(this).parent().find('p').hide('slow');
+  });
+  if ($(this).parent().find('p').css('display') == 'none')
+    $(this).parent().find('p').show('slow');
+  else $(this).parent().find('p').hide('slow');
+});
